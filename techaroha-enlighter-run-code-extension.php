cac9acb677ad4fbc4d9cf8bda12a82887fc44cbf
manojkolhe <manojkumar.kolhe@gmail.com>
2023-09-11 18:58:57 +0530

    Add comments for code documentation and clarity

--- techaroha-enlighter-run-code-extension.php
+++ techaroha-enlighter-run-code-extension.php
@@ -11,44 +11,61 @@ function is_jjavascript_css_working() {
     return file_exists(plugin_dir_path(__FILE__) . 'script-enlighter.js');
 }
 
-
 function add_api_key_to_header() {
+    // Retrieve the current API key stored in WordPress options.
     $current_api_key = get_option('techaroha_compiler_api_key'); 
-
+    // Check if the API key is not empty.
     if (!empty($current_api_key)) {
+         // Output a JavaScript variable in the HTML <head> section with the API key.
         echo '<script>var techaroha_compiler_api_key = "' . esc_js($current_api_key) . '";</script>';
     }
 }
+
+// Hook 'add_api_key_to_header' function to the 'wp_head' action.
 add_action('wp_head', 'add_api_key_to_header');
 
 function custom_plugin_menus() {
+    // Add an options page with a title and capability requirements.
     add_options_page('Custom Plugin Settings', 'Enlighter – Extension With Run Button', 'manage_options', 'custom-plugin', 'custom_plugin_pages');
 }
+
+// Hook 'custom_plugin_menus' function to the 'admin_menu' action.
 add_action('admin_menu', 'custom_plugin_menus');
+
 function custom_plugin_activation_redirecto() {
     if (is_admin() && get_option('activate_custom_plugin')) {
-        delete_option('activate_custom_plugin');
+        delete_option('activate_custom_plugin');  
         wp_safe_redirect(admin_url('options-general.php?page=custom-plugin'));
-        exit();
+        exit();  // Terminate script execution to ensure the redirection takes effect.
     }
 }
+
 register_activation_hook(__FILE__, 'custom_plugin_activation_redirecto');
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'custom_plugin_settings_link');
+
 function custom_plugin_settings_link($links) {
     $settings_link = '<a href="options-general.php?page=custom-plugin">Settings</a>';
     array_unshift($links, $settings_link);
     return $links;
 }
+
 function custom_plugin_pages() {
+    // Output HTML markup to create a page in the WordPress admin panel.
     echo '<div class="wrap">';
     echo '<h2>Enlighter – Extension With Run Button</h2>';
+    
+    // Check if the 'submit_api_key' form variable is set (indicating that the form has been submitted).
 	if (isset($_POST['submit_api_key'])) {
+        // Sanitize and store the submitted API key in the WordPress options.
         $api_key = sanitize_text_field($_POST['techaroha_compiler_api_key']);
         update_option('techaroha_compiler_api_key', $api_key);
     }
 
+     // Retrieve the current API key from the WordPress options.
     $current_api_key = get_option('techaroha_compiler_api_key', '');
-     echo '<form method="post">';
+
+    // Output a form for setting the Techaroha AI Online Compiler API Key.
+    echo '<form method="post">';
     echo '<table class="form-table">';
     echo '<tr valign="top">';
     echo '<th scope="row">Techaroha AI Online Compiler API Key</th>';
@@ -58,6 +75,7 @@ function custom_plugin_pages() {
     echo '<input type="submit" name="submit_api_key" value="Activate API Key" class="button-primary" />';
     echo '</form>';
 
+    // Output information about the API key and status.
     echo '<ul class="status-list">';
     echo '<li>';
     echo '<p class="info">'; 
@@ -78,60 +96,64 @@ function custom_plugin_pages() {
     echo '<p>The "RUN" button is now available on Enlighter consoles.</p>';
     echo '</div><br>';
 
+        // Output inline CSS styles for styling the page.
     echo '<style>
-.wrap {
-    padding: 20px !important;
-    background-color: #f5f5f5 !important;
-}
+        .wrap {
+            padding: 20px !important;
+            background-color: #f5f5f5 !important;
+        }
 
-h1, h2 {
-    margin-bottom: 20px !important;
-}
+        h1, h2 {
+            margin-bottom: 20px !important;
+        }
 
-.form-table {
-    width: 100% !important;
-    border-collapse: collapse !important;
-    margin-bottom: 30px !important;
-}
+        .form-table {
+            width: 100% !important;
+            border-collapse: collapse !important;
+            margin-bottom: 30px !important;
+        }
 
-.form-table th,
-.form-table td {
-    padding: 10px !important;
-    border: 1px solid #ccc !important;
-}
+        .form-table th,
+        .form-table td {
+            padding: 10px !important;
+            border: 1px solid #ccc !important;
+        }
 
-.status-list {
-    list-style: none !important;
-    padding: 0 !important;
-}
+        .status-list {
+            list-style: none !important;
+            padding: 0 !important;
+        }
 
-.status-list li {
-    margin-bottom: 10px !important;
-    display: flex !important;
-    align-items: center !important;
-}
+        .status-list li {
+            margin-bottom: 10px !important;
+            display: flex !important;
+            align-items: center !important;
+        }
 
-.status-list img {
-    width: 20px !important;
-    height: 20px !important;
-    margin-right: 10px !important;
-}
+        .status-list img {
+            width: 20px !important;
+            height: 20px !important;
+            margin-right: 10px !important;
+        }
 
-.status-ok {
-    color: green !important;
-}
+        .status-ok {
+            color: green !important;
+        }
 
-.status-not-ok {
-    color: red !important;
-}
-</style>';
-}
+        .status-not-ok {
+            color: red !important;
+        }
+        </style>';
+        }
 
 
 
 
 function custom_plugin_enqueue_scripts() {
+    // Enqueue a JavaScript file named 'custom-plugin-script' for use on WordPress pages.
     wp_enqueue_script('custom-plugin-script', plugins_url('script-enlighter.js', __FILE__), array('jquery'), '1.0', true);
 }
+
+// Add an action hook to enqueue the custom script when 'wp_enqueue_scripts' is triggered.
 add_action('wp_enqueue_scripts', 'custom_plugin_enqueue_scripts');
 ?>
