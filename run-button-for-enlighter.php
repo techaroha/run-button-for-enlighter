21343816d40ea4041bda4d9fd54c1321d464507b
Manoj Kolhe <manojkumar.kolhe@gmail.com>
2024-09-30 13:15:45 +0530

    update plugin as per wordpress guidelines

--- run-button-for-enlighter.php
+++ run-button-for-enlighter.php
@@ -11,27 +11,32 @@ License: GPL-2.0
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+    exit; // Exit if accessed directly
 
-function taenlighter_jscss_check() {
+function taenlighter_jscss_check()
+{
     return file_exists(plugin_dir_path(__FILE__) . 'script-enlighter.js');
 }
 
 
-function taenlighter_add_api_key_to_header() {
+function taenlighter_add_api_key_to_header()
+{
     // Retrieve the current API key stored in WordPress options.
-    $current_api_key = get_option('default_api_key'); 
+    $current_api_key = get_option('taenlighter_default_api_key');
     // Check if the API key is not empty.
     if (!empty($current_api_key)) {
-         // Output a JavaScript variable in the HTML <head> section with the API key.
-        echo '<script>var default_api_key = "' . esc_js($current_api_key) . '";</script>';
+        // Output a JavaScript variable in the HTML <head> section with the API key.
+        $script = 'var taenlighter_default_api_key = "' . esc_js($current_api_key) . '";';
+        wp_add_inline_script('custom-plugin-script', $script);
     }
 }
 
 // Hook 'add_api_key_to_header' function to the 'wp_head' action.
 add_action('wp_head', 'taenlighter_add_api_key_to_header');
 
-function taenlighter_plugin_menus() {
+function taenlighter_plugin_menus()
+{
     // Add an options page with a title and capability requirements.
     add_options_page('Custom Plugin Settings', 'Enlighter – Extension With Run Button', 'manage_options', 'custom-plugin', 'taenlighter_plugin_pages');
 }
@@ -39,7 +44,8 @@ function taenlighter_plugin_menus() {
 // Hook 'taenlighter_plugin_menus' function to the 'admin_menu' action.
 add_action('admin_menu', 'taenlighter_plugin_menus');
 
-function taenlighter_plugin_activation_redirecto() {
+function taenlighter_plugin_activation_redirecto()
+{
     if (is_admin() && get_option('taenlighter_activate_plugin')) {
         delete_option('taenlighter_activate_plugin');
         wp_safe_redirect(admin_url('options-general.php?page=custom-plugin'));
@@ -50,32 +56,34 @@ function taenlighter_plugin_activation_redirecto() {
 register_activation_hook(__FILE__, 'taenlighter_plugin_activation_redirecto');
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'taenlighter_plugin_settings_link');
 
-function taenlighter_plugin_settings_link($links) {
+function taenlighter_plugin_settings_link($links)
+{
     $settings_link = '<a href="options-general.php?page=custom-plugin">Settings</a>';
     array_unshift($links, $settings_link);
     return $links;
 }
 
-function taenlighter_plugin_pages() {
+function taenlighter_plugin_pages()
+{
     // Output HTML markup to create a page in the WordPress admin panel.
     echo '<div class="wrap">';
     echo '<h2>Enlighter – Extension With Run Button</h2>';
-    
+
     // Check if the 'submit_api_key' form variable is set (indicating that the form has been submitted).
     if (isset($_POST['submit_api_key'])) {
         // Verify nonce
         if (isset($_POST['submit_api_key_nonce']) && wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['submit_api_key_nonce'])), 'submit_api_key_nonce')) {
             // Sanitize and store the submitted API key in the WordPress options.
-            $api_key = sanitize_text_field($_POST['default_api_key']);
-            update_option('default_api_key', $api_key);
+            $api_key = sanitize_text_field($_POST['taenlighter_default_api_key']);
+            update_option('taenlighter_default_api_key', $api_key);
         } else {
             // Nonce verification failed, handle accordingly (e.g., show an error message).
             echo '<div class="error"><p>Security check failed. Please try again.</p></div>';
         }
     }
 
-     // Retrieve the current API key from the WordPress options.
-    $current_api_key = get_option('default_api_key', '');
+    // Retrieve the current API key from the WordPress options.
+    $current_api_key = get_option('taenlighter_default_api_key', '');
 
     // Output a form for setting the Techaroha AI Online Compiler API Key.
     echo '<form method="post">';
@@ -84,7 +92,7 @@ function taenlighter_plugin_pages() {
     echo '<table class="form-table">';
     echo '<tr valign="top">';
     echo '<th scope="row">Techaroha AI Online Compiler API Key</th>';
-    echo '<td><input type="text" name="default_api_key" value="' . esc_attr($current_api_key) . '" /></td>';
+    echo '<td><input type="text" name="taenlighter_default_api_key" value="' . esc_attr($current_api_key) . '" /></td>';
     echo '</tr>';
     echo '</table>';
     echo '<input type="submit" name="submit_api_key" value="Activate API Key" class="button-primary" />';
@@ -93,82 +101,52 @@ function taenlighter_plugin_pages() {
     // Output information about the API key and status.
     echo '<ul class="status-list">';
     echo '<li>';
-    echo '<p class="info">'; 
-    echo '<b>API Key (Optional):</b> You can enter an API key here if you wish to enable the "Run Code" option with a different compiler powered by <a class="api-ref" style="color:blue;" href="https://techaroha.com/ai-compiler" target="_blank">Techaroha AI.</a> If no API key is provided, the code will be executed using the default Newtum API Compiler. Please note that if you enter an incorrect or invalid API key, it will result in an access error. Ensure the accuracy of your API key to ensure seamless code execution with your preferred compiler.'; 
-    echo '</p>'; 
+    echo '<p class="info">';
+    echo '<b>API Key (Optional):</b> You can enter an API key here if you wish to enable the "Run Code" option with a different compiler powered by <a class="api-ref" style="color:blue;" href="https://techaroha.com/ai-compiler" target="_blank">Techaroha AI.</a> If no API key is provided, the code will be executed using the default Newtum API Compiler. Please note that if you enter an incorrect or invalid API key, it will result in an access error. Ensure the accuracy of your API key to ensure seamless code execution with your preferred compiler.';
+    echo '</p>';
     echo '</li>';
     echo '<li>';
-    echo '<img src="' . esc_url(plugins_url('images/status-icon.png', __FILE__)) . '" alt="Status Icon" class="status-ok">';
+    echo '<img src="' . esc_url(plugins_url('./assets/images/status-icon.png', __FILE__)) . '" alt="Status Icon" class="status-ok">';
     echo esc_html__('Online Status', 'enlighter-extension-with-run-button') . ' - ' . esc_html__('OK', 'enlighter-extension-with-run-button');
     echo '</li>';
     echo '<li>';
-    echo '<img src="' . esc_url(plugins_url('images/status-icon.png', __FILE__)) . '" alt="Status Icon" class="' . (taenlighter_jscss_check() ? 'status-ok' : 'status-not-ok') . '">';
+    echo '<img src="' . esc_url(plugins_url('./assets/images/status-icon.png', __FILE__)) . '" alt="Status Icon" class="' . (taenlighter_jscss_check() ? 'status-ok' : 'status-not-ok') . '">';
     echo esc_html__('Javascript and CSS Working', 'enlighter-extension-with-run-button') . ' - ';
     echo taenlighter_jscss_check() ? esc_html__('OK', 'enlighter-extension-with-run-button') : esc_html__('Not Working', 'enlighter-extension-with-run-button');
     echo '</li>';
     echo '</ul>';
-    echo '<img src="' . esc_url(plugins_url('images/info-icon.png', __FILE__)) . '" alt="Info Icon" class="info-icon">';
+    echo '<img src="' . esc_url(plugins_url('./assets/images/info-icon.png', __FILE__)) . '" alt="Info Icon" class="info-icon">';
     echo '<p>The "RUN" button is now available on Enlighter consoles.</p>';
     echo '</div><br>';
 
-        // Output inline CSS styles for styling the page.
-    echo '<style>
-        .wrap {
-            padding: 20px !important;
-            background-color: #f5f5f5 !important;
-        }
+}
 
-        h1, h2 {
-            margin-bottom: 20px !important;
-        }
+function taenlighter_runbutton_enqueue_styles()
+{
+    wp_enqueue_style('enlighter-extension-style', plugins_url('./assets/css/style-enlighter.css', __FILE__));
+}
 
-        .form-table {
-            width: 100% !important;
-            border-collapse: collapse !important;
-            margin-bottom: 30px !important;
-        }
+add_action('wp_enqueue_scripts', 'taenlighter_runbutton_enqueue_styles');
 
-        .form-table th,
-        .form-table td {
-            padding: 10px !important;
-            border: 1px solid #ccc !important;
-        }
+add_filter('script_loader_tag', 'taenlighter_add_nonce_to_script', 10, 2);
 
-        .status-list {
-            list-style: none !important;
-            padding: 0 !important;
-        }
-
-        .status-list li {
-            margin-bottom: 10px !important;
-            display: flex !important;
-            align-items: center !important;
-        }
-
-        .status-list img {
-            width: 20px !important;
-            height: 20px !important;
-            margin-right: 10px !important;
-        }
-
-        .status-ok {
-            color: green !important;
-        }
-
-        .status-not-ok {
-            color: red !important;
-        }
-        </style>';
-        }
+function taenlighter_add_nonce_to_script($tag, $handle) {
+    if ('custom-plugin-script' === $handle) {
+        return str_replace('src', 'nonce="' . wp_create_nonce('custom_nonce') . '" src', $tag);
+    }
+    return $tag;
+}
 
 
-
-
-function taenlighter_plugin_enqueue_scripts() {
+function taenlighter_runbutton_enqueue_scripts()
+{
     // Enqueue a JavaScript file named 'custom-plugin-script' for use on WordPress pages.
     wp_enqueue_script('custom-plugin-script', plugins_url('script-enlighter.js', __FILE__), array('jquery'), '1.0', true);
+    // Add async attribute to the script
+    wp_script_add_data('custom-plugin-script', 'async', true);
+    
 }
 
 // Add an action hook to enqueue the custom script when 'wp_enqueue_scripts' is triggered.
-add_action('wp_enqueue_scripts', 'taenlighter_plugin_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'taenlighter_runbutton_enqueue_scripts');
 ?>
